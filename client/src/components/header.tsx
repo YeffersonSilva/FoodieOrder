c42cce15f4a9341a1d946c7e409301482621495b
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-08-07 17:33:40 -0300

    refactor: Add navigation menu to header component

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,4 +1,6 @@
+import Link from "next/link";
 import Logo from "./logo";
+import { menu } from "@/assets/data";
 
 export const Header = () => {
     return ( 
@@ -6,7 +8,24 @@ export const Header = () => {
             <section >
                 <Logo/>
             </section>
-            <section>Nav</section>
+            <section className="hidden lg:block">
+          <nav>
+            <ul className="flex items-center gap-x-1 bg-background-foreground p-1 rounded-full">
+              {menu.map((menu) => (
+                <li key={menu.href}>
+                  <Link
+                    href={menu.href}
+                    className={
+                      "flex items-center gap-x-2 text-sm py-3 px-6 rounded-full transition-colors"}
+                  >
+                    <i className={menu.icon}></i>
+                    {menu.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </section>
             <section>User</section>
         </header>
      );
