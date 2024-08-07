1a33d663848637f673677750ea26677c8d8ba044
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-08-07 17:46:48 -0300

    chore: Update header component with dynamic menu styling

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Logo from "./logo";
 import { menu } from "@/assets/data";
+import { cn } from "@/lib/utils";
 
 export const Header = () => {
     return ( 
@@ -15,8 +16,12 @@ export const Header = () => {
                 <li key={menu.href}>
                   <Link
                     href={menu.href}
-                    className={
-                      "flex items-center gap-x-2 text-sm py-3 px-6 rounded-full transition-colors"}
+                    className={cn(
+                      "flex items-center gap-x-2 text-sm py-3 px-6 rounded-full transition-colors",
+                      menu.href === "/menu"
+                        ? "bg-primary font-medium"
+                        : "hover:bg-background/70"
+                    )}
                   >
                     <i className={menu.icon}></i>
                     {menu.label}
@@ -26,7 +31,13 @@ export const Header = () => {
             </ul>
           </nav>
         </section>
-            <section>User</section>
+            <section>
+            <ul className="flex items-center gap-x-3 bg-background-foreground p-3 rounded-full">
+                    <li className="flex items-center gap-x-2">
+                        
+            </li>
+            </ul>
+            </section>
         </header>
      );
 }
